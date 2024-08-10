31ab865cabbc471137fb3173303c5d9fd4c00d8d
Santhosh-2023106 <santhosh@tivonaglobal.com>
2024-08-11 00:02:04 +0530

    new -v

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is our ECS deployment using pipeline.
         </a>
       </header>
     </div>
